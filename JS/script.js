274889cc7a0a8bb4f2beebd3bdcc8dd252a13e25
boris94kg <boris94serbia@gmail.com>
2019-07-30 17:42:03 +0200

    added owl carousel

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,14 @@
-$(document).ready(function(){
+//Background image change
+$(document).ready(function () {
   $('#slides').superslides({
     animation: 'fade',
     play: 5000,
     pagination: false
   });
 
+  // Typing heading
   var typingElement = document.querySelector('.typed');
-  var typed = new Typed(typingElement,{
+  var typed = new Typed(typingElement, {
     strings: ['Junior Front-End Developer'],
     typeSpeed: 70,
     startDelay: 500,
@@ -17,3 +19,20 @@ $(document).ready(function(){
 
 
 
+//Carouses
+$('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 5
+    }
+  }
+})

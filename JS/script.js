58c37a23fc9b244fe887fba8cf312a0cf472601a
boris94kg <boris94serbia@gmail.com>
2019-07-30 16:02:25 +0200

    done with headings

--- JS/script.js
+++ JS/script.js
@@ -3,9 +3,16 @@ $(document).ready(function(){
     animation: 'fade',
     play: 5000,
     pagination: false
-  }
+  });
 
-  );
+  var typingElement = document.querySelector('.typed');
+  var typed = new Typed(typingElement,{
+    strings: ['Junior Front-End Developer'],
+    typeSpeed: 70,
+    startDelay: 500,
+    showCursor: false
+  });
+
 })
 
 

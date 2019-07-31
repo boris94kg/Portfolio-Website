6b32260309ecadaef8b46f930ce569f118e56ba1
boris94kg <boris94serbia@gmail.com>
2019-07-31 21:56:33 +0200

    added section with projects

--- JS/script.js
+++ JS/script.js
@@ -69,16 +69,42 @@ $(document).ready(function () {
   });
 
 
-    // Counters for projects and websites
+$('[data-fancybox]').fancybox();
 
-  // $('.counter').each(function(){
-  //   var element = $(this);
-  //   var endVal = Number.parseInt(element.text());
 
-  //   element.countup(endVal);
-  // })
+$('.items').isotope({
+  filter: '*',
+  animationOptions:{
+    duration: 1500,
+    easing: 'linear',
+    queue: false
+  }
+})
+
+$('.filters a').click(function(){
+    $('#filters .current').removeClass('current');
+    $(this).addClass('current');
+
+    var selector = $(this).attr('data-filter');
+
+    $('.items').isotope({
+      filter: selector,
+      animationOptions:{
+        duration: 1500,
+        easing: 'linear',
+        queue: false
+      }
+    });
+
+    return false;
+
+
+
+});
+
+
 
 
 })
-
+ 
 
